98818652e2e76fdb932c913759b9eb49faf70f8a
Eugene Viktorov <eugene.viktorov@anvil8.com>
2015-12-02 16:53:23 +0200

    refactor: set complitable python2/3 setup.py logic

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
+import sys
 from setuptools import setup
+
+
+required_packages = ['requests', 'btctxstore']
+if sys.version_info.major == 2:
+    required_packages.insert(0, 'mock')
 
 setup(
     name='metatool',
     version='1.0',
     packages=['metatool'],
-    install_requires=[
-        'requests',
-        'btctxstore',
-    ],
+    install_requires=required_packages,
     test_suite='metatool.test_metadisk',
     entry_points={
         'console_scripts':
