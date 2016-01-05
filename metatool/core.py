157ddb175f58256b4c07373a1bcebf43f826ff34
Eugene Viktorov <eugene.viktorov@anvil8.com>
2016-01-05 20:10:13 +0200

    refactor: core.py docstrings with respect to "rst" syntax

--- metatool/core.py
+++ metatool/core.py
@@ -1,3 +1,6 @@
+"""
+Core MetaTool functions.
+"""
 import sys
 import os
 import os.path
@@ -13,25 +16,32 @@ else:
 
 
 def audit(url_base, sender_key, btctx_api, file_hash, seed):
-    """
-    It performs an request to the server with a view of calculating
+    """It performs an request to the server with a view of calculating
     the SHA-256 hash of a file plus some seed.
-    It will return the response object with information about the server-error
+    Return the response object with information about the server-error
     when such has occurred.
 
-    :param url_base:  (str) URL string which defines the server will be used.
-    :param sender_key: (str) unique secret key which will be used for the
-                        generating credentials required by the access to the
-                        server.
-    :param btctx_api:  instance of the BtcTxStore class from btctxstore module.
-                        It will be used to generate credentials for server
-                        access. (https://pypi.python.org/pypi/btctxstore/4.6.0)
-    :param file_hash:  (str) hash-name of the file you are going to audit.
+    :param url_base: URL string which defines the server will be used
+    :type url_base: string
+
+    :param sender_key: unique secret key which will be used for the
+        generating credentials required by the access to the server
+    :type sender_key: string
+
+    :param btctx_api: instance of the ``BtcTxStore`` class which will be used
+        to generate credentials for the server access
+    :type btctx_api: btctxstore.BtcTxStore object
+
+    :param file_hash: hash-name of the file you are going to audit
+    :type file_hash: string
+
     :param seed: (str) should be a SHA-256 hash of that you would like to add
-                        to the file data to generate a challenge response.
-    :return: response: (<class 'requests.models.Response'>) response instance
-                        with the results of challenge or with information about
-                        the server issue.
+        to the file data to generate a challenge response
+    :type seed: string
+
+    :returns: response instance with the results of challenge or with
+        information about the server issue
+    :rtype: requests.models.Response object
     """
     signature = btctx_api.sign_unicode(sender_key, file_hash)
     sender_address = btctx_api.get_address(sender_key)
@@ -53,39 +63,53 @@ def download(url_base, sender_key, btctx_api, file_hash,
              rename_file=None, decryption_key=None, link=False):
     """
     It performs the downloading of the file from the server
-    by the given "file_hash".
-    If "link=True" will return GET-request URL-string instead of performing the
-    download, so you can use it like a simple URL address and download it
-    through the browser.
-    note:: The "link=True" case implies non-authenticated access to the
-    file, available only for files with such values of "roles": 101, 001
+    by the given ``file_hash``.
+    If ``link=True`` will return GET-request URL-string instead of download,
+    so you can use it like a simple URL address and download it through the
+    browser.
+
+    :note: The "link=True" case implies non-authenticated access to the
+        file, available only for files with such values of ``roles``: 101, 001
 
     Will return the response object with information about the server-error
     when such has occurred.
 
-    :param url_base:  (str) URL string which defines the server will be used.
-    :param sender_key: (str) unique secret key which will be used for the
-                        generating credentials required by the access to the
-                        server.
-    :param btctx_api:  instance of the BtcTxStore class from btctxstore module.
-                        It will be used to generate credentials for server
-                        access. (https://pypi.python.org/pypi/btctxstore/4.6.0)
-    :param file_hash:  (str) hash-name of the file on the remote server.
-    :param rename_file:  (str) defines new name of the file after downloading.
-    :param decryption_key:  (str)  key value which will be used to decrypt file
-                            on the server before the downloading (if allowed by
-                            the role).
-    :param link: (boll) If True is given, function will generate and return
-                        the appropriate GET URL-string instead of performing
-                        the downloading process.
-    :return:
-        <file_path>: (str) will return full path to the file if downloaded
-                            done successfully.
-        <GET URL-string>: (str) if "link=True" will return GET-request
-                            URL-string, instead of processing the downloading.
-        response_object: (<class 'requests.models.Response'>) with information
-                            about the occurred error on the server while
-                            the downloading.
+    :param url_base: URL-string which defines the server will be used
+    :type url_base: string
+
+    :param sender_key: unique secret key which will be used for the
+        generating credentials required by the access to the server
+    :type sender_key: string
+
+    :param btctx_api: instance of the ``BtcTxStore`` class which will be used
+        to generate credentials for the server access
+    :type btctx_api: btctxstore.BtcTxStore object
+
+    :param file_hash: hash-name of the file you are going to audit
+    :type file_hash: string
+
+    :param rename_file: defines new name of the file after downloading
+    :type rename_file: string
+
+    :param decryption_key: key value which will be used to decrypt file
+        on the server before the downloading (if allowed by the role)
+    :type decryption_key: string
+
+    :param link: if ``True``, function doesn't perform the downloading but
+        generate appropriate GET URL-string instead
+    :type link: boolean
+
+    :returns: full path to the file if download done successfully
+
+        :rtype: string
+    :returns: if ``link=True``, will return GET-request
+        URL-string, instead of processing the downloading
+
+        :rtype: string
+    :returns: object with information about the occurred error on the server
+        while the downloading
+
+        :rtype: requests.models.Response object
     """
     url_for_requests = urljoin(url_base, '/api/files/' + file_hash)
 
@@ -132,22 +156,30 @@ def upload(url_base, sender_key, btctx_api, file, file_role):
     Core method of the METATOOL API which uploads local file to the server.
     Max size of file is determined by the server. In the most of cases it is
     restricted by the 128 MB.
-    Will return the response object with information about the server-error
-    when such has occurred.
+    Will return the response object with ``file_hash`` and ``file_role`` when
+    have done successfully or the server-error when such has occurred.
 
-    :param url_base:  (str) URL string which defines the server will be used.
-    :param sender_key: (str) unique secret key which will be used for the
-                        generating credentials required by the access to the
-                        server.
-    :param btctx_api:  instance of the BtcTxStore class from btctxstore module.
-                        It will be used to generate credentials for server
-                        access. (https://pypi.python.org/pypi/btctxstore/4.6.0)
-    :param file:  file object opened in the 'rb' mode,
-                        which will be uploaded to the server.
-    :param file_role:  (str)  three numbers string which define future
-                        behavior of the file on the server. See more about
-                        it in the documentation.
-    :return: response: (<class 'requests.models.Response'>) response instance.
+    :param url_base: URL-string which defines the server will be used
+    :type url_base: string
+
+    :param sender_key: unique secret key which will be used for the
+        generating credentials required by the access to the server
+    :type sender_key: string
+
+    :param btctx_api: instance of the ``BtcTxStore`` class which will be used
+        to generate credentials for the server access
+    :type btctx_api: btctxstore.BtcTxStore object
+
+    :param file: file object which will be uploaded to the server
+    :type file: file object opened in the 'rb' mode
+
+    :param file_role: three numbers string which define future behavior of the
+        file on the server. See more about it in the documentation
+    :type file_role: string
+
+    :returns: response instance with the results of uploading or with
+        information about the server issue
+    :rtype: requests.models.Response object
     """
     files_header = {'file_data': file}
     data_hash = sha256(file.read()).hexdigest()
@@ -174,9 +206,12 @@ def files(url_base):
     It executes a request to the "Node" server and returns response object
     with json-string - the list of hashes of downloaded files.
 
-    :param url_base: (str) URL string which defines the server
-                        that will be used.
-    :return: response object
+    :param url_base: URL-string which defines the server will be used
+    :type url_base: string
+
+    :returns: response instance with a list of string - hashes of files
+        available on the server or with information about the server issue
+    :rtype: requests.models.Response object
     """
     response = requests.get(urljoin(url_base, '/api/files/'))
     return response
@@ -185,10 +220,14 @@ def files(url_base):
 def info(url_base):
     """
     It executes a request to the "Node" server and returns response object
-    with json-string - the information about server state.
+    with json-string - the information about server application state.
 
-    :param url_base: (str) URL string which defines the server
-                        that will be used.
+    :param url_base: URL-string which defines the server will be used
+    :type url_base: string
+
+    :returns: response instance with node state information or with
+        information about the server issue
+    :rtype: requests.models.Response object
     """
     response = requests.get(urljoin(url_base, '/api/nodes/me/'))
     return response
